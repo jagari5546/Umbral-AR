83bbcb0709cab9ad93701ea75e9c199c4b29dcce
jagari5546 <122942343+jagari5546@users.noreply.github.com>
2025-05-24 01:17:03 -0500

    V0.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,40 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
+    [SerializeField] private float WaitTime = 1f;
+    [SerializeField] private GameObject MomentosIcon;
+    [SerializeField] private GameObject MujerBarandaIcon;
+    [SerializeField] private GameObject LaEncomiendaIcon;
+    [SerializeField] private GameObject ExitButton;
+    [SerializeField] private GameObject Momentos;
+    [SerializeField] private GameObject MujerBaranda;
+    [SerializeField] private GameObject LaEncomienda;
+    [SerializeField] private GameObject PanelDesplegable;
+    [SerializeField] private Animator PanelDesplegableAnimator;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-
-    void Start()
+    public void CerrarPanelDesplegable()
     {
-        
+        PanelDesplegableAnimator.SetTrigger("Close");
+        PanelDesplegableAnimator.ResetTrigger("Open");
+        StartCoroutine(EsperarUnSegundo());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator EsperarUnSegundo()
     {
-        
+        yield return new WaitForSeconds(WaitTime); 
+        Debug.Log("funciones despues de esperar");
+        MomentosIcon.SetActive(true);
+        MujerBarandaIcon.SetActive(true);
+        LaEncomiendaIcon.SetActive(true);
+        ExitButton.SetActive(true);
+        MujerBaranda.SetActive(false);
+        LaEncomienda.SetActive(false);
+        Momentos.SetActive(false);
+        PanelDesplegable.SetActive(false);
     }
 
     
