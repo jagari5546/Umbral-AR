11c1fd07792afc6d03a7baf94b4eb2d24eec3793
jagari5546 <122942343+jagari5546@users.noreply.github.com>
2025-05-24 00:24:29 -0500

    ScrollView

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] ModelsPanels;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
 
     void Start()
     {
-        foreach (var panel in ModelsPanels)
-        {
-            gameObject.SetActive(false);
-        }
+        
     }
 
     // Update is called once per frame
